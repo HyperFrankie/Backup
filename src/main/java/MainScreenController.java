8e6d0e9e24bece17e374feb6bbe43f277d8b879d
lenardjee <lenardvdmaas@hotmail.nl>
2019-02-01 14:49:56 +0100

    test with TileFX

--- src/main/java/MainScreenController.java
+++ src/main/java/MainScreenController.java
@@ -1,11 +1,15 @@
 import eu.hansolo.tilesfx.Tile;
+import eu.hansolo.tilesfx.chart.ChartData;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
-import eu.hansolo.tilesfx.skins.
+import java.time.Instant;
+import java.util.ArrayList;
 
 public class MainScreenController {
 
@@ -22,7 +26,18 @@ public class MainScreenController {
         homeButton.setImage(image);
         backupTasksButton.setImage(image);
         currentStateButton.setImage(image);
-        mainScreenCenterPane.getChildren().add((new Tile(Tile.SkinType.DONUT_CHART)));
+
+        Tile testTile = new Tile(Tile.SkinType.BAR_CHART);
+        testTile.setTitle("Test Title");
+        testTile.setTitleAlignment(TextAlignment.CENTER);
+        testTile.addChartData(new ArrayList<ChartData>() {{
+            add(new ChartData("Data 1", 14.1d, Color.GREENYELLOW));
+            add(new ChartData("Data 2", 23.5d, Color.AQUA));
+        }});
+        testTile.getChartData().get(0).setAnimated(true);
+        testTile.setPrefSize(300, 300);
+        testTile.setAutoScale(true);
+        mainScreenCenterPane.getChildren().add(testTile);
     }
 
 }
